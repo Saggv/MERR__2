304328fc3d23387f4863a44e6e241944464aab8c
Your Name <you@example.com>
2019-08-26 18:53:33 +0700

    push hotel project to heroku

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+require('dotenv').config();
 const express =require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const path = require('path');
 
 //import router
 const Room = require("./Router/Rooms");
@@ -15,11 +17,10 @@ app.use(express.json());
 
 //Connect to mongoose
 
-mongoose.connect("mongodb://localhost/hotel", {useNewUrlParser:true,useFindAndModify:false})
+mongoose.connect(process.env.MongooseURL, {useNewUrlParser:true,useFindAndModify:false})
         .then( ()=>console.log("Connect with mongoose success...."))
         .catch( (err)=>console.log(err))
 
-const PORT = 7000;
 
 app.use('/rooms',Room);
 app.use('/user', Users)
@@ -28,4 +29,14 @@ app.get('/', (req, res)=>{
        res.send("hello");
 })
 
-app.listen(PORT, ()=>console.log(`this server is running in port ${PORT}`));
+if(process.env.NODE_ENV ==='production'){
+        //set static folder
+        app.use( express.static('hotel/build'));
+
+        app.get('*', (req, res)=>{
+                 res.sendFile(path.resolve(__dirname, 'hotel', 'build', 'index.html'))
+        })
+}
+const port = process.env.PORT || 7000;
+
+app.listen(port, ()=>console.log(`this server is running in port ${port}`));
